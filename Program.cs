bb309f9b51a480293d22c2edb02814ce3750dcf1
Bhagu13 <71821486+Bhagu13@users.noreply.github.com>
2024-03-24 22:11:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("Model Y", "Tesla", 2022, 100.0, 5, "Electric", "Automatic", false);
+        Truck truck = new Truck("Sierra HD", "GMC", 2024, 150.0, 2.5, "Pickup", true);
+        Motorcycle motorcycle = new Motorcycle("Fat Boy 114", "Harley Davidson", 2023, 70.0, 1000, "Petrol", true);
+
+        RentalAgency rentalAgency = new RentalAgency(10);
+
+        rentalAgency.AddVehicle(car);
+        rentalAgency.AddVehicle(truck);
+        rentalAgency.AddVehicle(motorcycle);
+
+        rentalAgency.DisplayFleetDetails();
+
+        rentalAgency.RentVehicle(car, 7);
+
+        rentalAgency.DisplayFleetDetails();
+
+        Console.ReadKey();
+    }
+}
---
+++ RentalAgency.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class RentalAgency
+{
+    // Array to store fleet of vehicles
+    private Vehicle[] Fleet;
+    private int count;
+    // Property to track total revenue
+    public double TotalRevenue { get; private set; }
+
+    public RentalAgency(int capacity)
+    {
+        Fleet = new Vehicle[capacity];
+        count = 0;
+        TotalRevenue = 0;
+    }
+
+    // Method to add vehicle to fleet
+    public void AddVehicle(Vehicle vehicle)
+    {
+        if (count < Fleet.Length)
+        {
+            Fleet[count++] = vehicle;
+        }
+        else
+        {
+            Console.WriteLine("Fleet is full, cannot add more vehicles.");
+        }
+    }
+
+    // Method to remove vehicle from fleet
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        int index = Array.IndexOf(Fleet, vehicle);
+        if (index != -1)
+        {
+            Fleet[index] = null;
+            count--;
+        }
+    }
+
+    // Method to rent a vehicle
+    public void RentVehicle(Vehicle vehicle, int days)
+    {
+        double rentalCost = vehicle.RentalPrice * days;
+        TotalRevenue += rentalCost;
+        Console.WriteLine($"Vehicle rented: {vehicle.Model} for {days} days. Total cost: ${rentalCost}");
+        RemoveVehicle(vehicle);
+    }
+
+    public void DisplayFleetDetails()
+    {
+        Console.WriteLine("Fleet Details:");
+        foreach (Vehicle vehicle in Fleet)
+        {
+            if (vehicle != null)
+            {
+                vehicle.DisplayDetails();
+                Console.WriteLine();
+            }
+        }
+    }
+}
